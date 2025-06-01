db1a054abb9530da8a978cdbacc3a4e794535893
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2025-06-01 18:36:07 -0400

    error 🚫

---
+++ components/ui/typography.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { H1 } from './typography';
+
+const meta = {
+  title: 'H1',
+  component: H1,
+} satisfies Meta<typeof H1>;
+
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const BaseComponentName: Story = {
+  args: {
+  },
+};
